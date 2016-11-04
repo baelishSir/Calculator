b590c7f6db8d134618f447e64d69a3c6ddbd1fe2
baelishSir <sylvain.lerest@viacesi.fr>
2016-11-04 09:08:07 +0100

    Commute addition of two integers

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    printf("Hello world!\n");
+int addition(int a, int b){
+    return a+b;
+}
+int main(){
+    printf("%d\n", addition(1,4));
     return 0;
 }
